0e2d385b40a70bf8f861a456f61c93efa76920ca
Caio Queiroz <caio.psq@gmail.com>
2025-11-11 09:31:54 -0300

    Adicionando novas imagens ao carrossel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,16 +2,30 @@ import { useState, useEffect } from "react"
 import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
 
 export default function DestaqueCarousel() {
-    const imagens = [
-        "https://images.unsplash.com/photo-1601599967100-f16100982063?q=80&w=1364&auto=format&fit=crop",
-        "https://images.unsplash.com/photo-1593642634315-48f5414c3ad9?q=80&w=1364&auto=format&fit=crop",
-        "https://images.unsplash.com/photo-1522205408450-add114ad53fe?q=80&w=1364&auto=format&fit=crop",
+    const slides = [
+        {
+            src: "/carousel/cadeiras.jpeg",
+            texto: "Jogos de Mesa",
+        },
+        {
+            src: "/carousel/freezer.jpeg",
+            texto: "Freezer Horizontal",
+        },
+        {
+            src: "/carousel/frigobar.jpeg",
+            texto: "Frigobar",
+        },
+        {
+            src: "/carousel/geladeiraFrost.jpeg",
+            texto: "Geladeira Consul Frost Free 342 Litros",
+        },
     ]
+
     const [index, setIndex] = useState(0)
 
     useEffect(() => {
         const timer = setInterval(() => {
-            setIndex((prev) => (prev + 1) % imagens.length)
+            setIndex((prev) => (prev + 1) % slides.length)
         }, 4000)
         return () => clearInterval(timer)
     }, [])
@@ -19,10 +33,10 @@ export default function DestaqueCarousel() {
     return (
         <div className="relative">
             <div className="aspect-square rounded-2xl overflow-hidden bg-muted relative w-full h-full">
-                {imagens.map((src, i) => (
+                {slides.map((slide, i) => (
                     <img
                         key={i}
-                        src={src}
+                        src={slide.src}
                         alt={`Imagem ${i + 1}`}
                         className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-700 ease-in-out ${i === index ? "opacity-100 z-10" : "opacity-0 z-0"
                             }`}
@@ -30,8 +44,8 @@ export default function DestaqueCarousel() {
                 ))}
             </div>
 
-            <div className="absolute z-20 -bottom-6 -left-6 bg-primary text-primary-foreground p-6 rounded-xl">
-                <p className="text-sm">A melhor escolha para o seu evento.</p>
+            <div className="absolute z-20 -bottom-6 -left-6 bg-primary text-primary-foreground p-6 rounded-xl transition-opacity duration-700 ease-in-out">
+                <p className="text-sm">{slides[index].texto}</p>
             </div>
         </div>
     )
