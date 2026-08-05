1de54c44706ad1d2fab467081c1d9f3aebc2bb39
Caio Queiroz <caio.psq@gmail.com>
2026-08-05 13:53:49 -0300

    Duplicando carrossel, invertendo ordem e direção

--- src/components/infinite_carrousel/Carousel.tsx
+++ src/components/infinite_carrousel/Carousel.tsx
@@ -11,7 +11,7 @@ export default function Carousel() {
                 </h2>
                 <section className="flex h-screen justify-center items-center w-[300]" style={{ 'background-color': '#dcdddf' }}>
                     <div className="h-100 max-h-lvh w-full  rouded-2xl py-16 overflow-hidden grid">
-                        <Marquee gradient gradientColor="#dcdddf" pauseOnHover="true" className="rounded-2xl  py-3 overflow-hidden grid  ">
+                        <Marquee gradient pauseOnClick="true" gradientColor="#dcdddf" className="rounded-2xl  py-3 overflow-hidden grid  ">
                             <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
                                 <img src="/img/clientes/logoqualifoco.png" className="" style={{ height: '100px', padding: '0 50px' }} alt="qualifoco" />
                             </div>
@@ -58,6 +58,63 @@ export default function Carousel() {
 
                             <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
                                 <img src="/img/clientes/logo_senac.svg" className="w-40 " style={{ height: '100px', padding: '0 50px' }} alt="Senac" />
+                            </div>
+
+                        </Marquee>
+
+                    </div>
+
+                </section>
+                <section className="flex h-screen justify-center items-center w-[300]" style={{ 'background-color': '#dcdddf' }}>
+                    <div className="h-100 max-h-lvh w-full  rouded-2xl py-16 overflow-hidden grid">
+                        <Marquee gradient pauseOnClick="true" direction="right" gradientColor="#dcdddf" className="rounded-2xl  py-3 overflow-hidden grid  ">
+                            
+                            <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
+                                <img src="/img/clientes/logo_senac.svg" className="w-40 " style={{ height: '100px', padding: '0 50px' }} alt="Senac" />
+                            </div>
+                            
+                            <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
+                                <img src="/img/clientes/logoqualifoco.png" className="" style={{ height: '100px', padding: '0 50px' }} alt="qualifoco" />
+                            </div>
+
+                            <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
+                                <img src="/img/clientes/fgv_logo.svg" className="w-40 " style={{ height: '100px', padding: '0 50px' }} alt="FGV" />
+                            </div>
+
+                            <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
+                                <img src="/img/clientes/fnds-logo.png" className="w-40 " style={{ height: '100px', padding: '0 50px' }} alt="FNDS" />
+                            </div>
+
+                            <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
+                                <img src="/img/clientes/worldgym.png" className="w-40" style={{ height: '150px', padding: '0 50px' }} alt="World Gym" />
+                            </div>
+
+                            <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
+                                <img src="/img/clientes/Logo-piracicabana-df.png" className="w-40 " style={{ height: '100px', padding: '0 50px' }} alt="Viação Piracicabana" />
+                            </div>
+
+                            <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
+                                <img src="/img/clientes/agepol_logo.png" className="w-40 " style={{ height: '100px', padding: '0 50px' }} alt="AGEPOL" />
+                            </div>
+
+                            <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
+                                <img src="/img/clientes/assefaz_logo.svg" className="w-40 " style={{ height: '100px', padding: '0 50px' }} alt="Assefaz" />
+                            </div>
+
+                            <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
+                                <img src="/img/clientes/Lake_side_logo.png" className="w-40 " style={{ height: '100px', padding: '0 50px' }} alt="Condomínio Lake Side" />
+                            </div>
+
+                            <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
+                                <img src="/img/clientes/RedeDor_logo.svg" className="w-40 " style={{ height: '100px', padding: '0 50px' }} alt="Rede D'or" />
+                            </div>
+
+                            <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
+                                <img src="/img/clientes/logo_AgroBrasilia.png" className="w-40 " style={{ height: '100px', padding: '0 50px' }} alt="Agro Brasília" />
+                            </div>
+
+                            <div className=" flex pr-10 flex-col justify-center items-center h-[350px] mx-5 ">
+                                <img src="/img/clientes/cmw_logo.avif" className="w-40 " style={{ height: '100px', padding: '0 50px' }} alt="Capital Moto Week" />
                             </div>
 
                         </Marquee>
